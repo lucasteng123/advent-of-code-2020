d86530181f168e5dab1f0eae680f152f27cd9caa
Lucas Teng <lucas@igloovision.com>
2020-12-02 13:09:05 -0500

    cleanup day 2

--- src/day2.js
+++ src/day2.js
@@ -2,41 +2,46 @@
 // ======
 
 function part1(input) {
-    const passwords = input.split('\n');
+    const passwords = parseInput(input);
     let validPasswords = [];
     for(let pass of passwords) {
-        const sections = pass.split(' ');
-        const validRange = sections[0].split('-').map(x=>+x);
-        const letter = sections[1].slice(0,sections[1].length-1);
-        const p = sections[2];
-        let r = new RegExp(letter,'gi');
-        let results = [];
-        while(r.exec(p)){
-            results.push(r.lastIndex);
-        }
-        if(results.length >= validRange[0] && results.length <= validRange[1]) validPasswords.push(p);
+        let results = parsePassword(pass.password,pass.letter);
+        if(results.length >= pass.range[0] && results.length <= pass.range[1]) validPasswords.push(pass.password);
     }
     return validPasswords.length;
+}
+
+function parseInput(input){
+    const passwords = input.split('\n');
+    return passwords.map(pass => {
+        const sec = pass.split(' ');
+        return {
+            password: sec[2],
+            range: sec[0].split('-').map(x=>+x),
+            letter: sec[1].slice(0,sec[1].length-1)
+        }
+    });
+}
+
+function parsePassword(pass,letter){
+    let r = new RegExp(letter,'gi');
+    let results = [];
+    while(r.exec(pass)){
+        results.push(r.lastIndex);
+    }
+    return results;
 }
 
 // Part 2
 // ======
 
 function part2(input) {
-    const passwords = input.split('\n');
+    const passwords = parseInput(input);
     let validPasswords = [];
     for(let pass of passwords) {
-        const sections = pass.split(' ');
-        const validRange = sections[0].split('-').map(x=>+x);
-        const letter = sections[1].slice(0,sections[1].length-1);
-        const p = sections[2];
-        let r = new RegExp(letter,'gi');
-        let results = [];
-        while(r.exec(p)){
-            results.push(r.lastIndex);
-        }
+        const results = parsePassword(pass.password,pass.letter);
         //sorry
-        if(results.includes(validRange[0]) != results.includes(validRange[1])) validPasswords.push(p);
+        if(results.includes(pass.range[0]) != results.includes(pass.range[1])) validPasswords.push(pass.password);
     }
     return validPasswords.length;
 }
