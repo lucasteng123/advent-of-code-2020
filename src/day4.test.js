83bdc8a21516fbe4228009b957c7ac807fb287be
Lucas Teng <lucas@igloovision.com>
2020-12-04 01:51:14 -0500

    update tests

--- src/day4.test.js
+++ src/day4.test.js
@@ -1,7 +1,7 @@
 import * as day from './day4.js';
 const testInput = 'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\nhcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm\n\nhcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in';
 test('1', () => {
-    expect(day.part1(testInput)).toBe(1);
+    expect(day.part1(testInput)).toBe(2);
 });
 const invalidPassports = `eyr:1972 cid:100
 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
@@ -17,7 +17,7 @@ hgt:59cm ecl:zzz
 eyr:2038 hcl:74454a iyr:2023
 pid:3556412378 byr:2007`;
 test('invalidPass',()=>{
-    expect(day.part1(invalidPassports)).toBe(0);
+    expect(day.part2(invalidPassports)).toBe(0);
 });
 
 const validPassports = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
@@ -33,5 +33,5 @@ eyr:2022
 
 iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`;
 test('validPass',()=>{
-    expect(day.part1(validPassports)).toBe(4);
+    expect(day.part2(validPassports)).toBe(4);
 });
